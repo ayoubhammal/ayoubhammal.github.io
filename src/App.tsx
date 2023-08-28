d3742e39ce00c3927d238bea79e91cef2584cef5
Ayoub Hammal <ayoubhammal@gmail.com>
2023-08-28 13:36:48 +0200

    component lazy loading + typo article

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import type { Component } from 'solid-js';
+import { lazy } from 'solid-js';
 import { Router, Route, Routes, A } from "@solidjs/router";
 import styles from './style/App.module.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Curriculum from './components/Curriculum';
-import Blog from './components/Blog';
-import Article from './components/Article';
+
+const Curriculum = lazy(() => import('./components/Curriculum'));
+const Article = lazy(() => import("./components/Article"));
+const Blog = lazy(() => import( './components/Blog'));
+
 
 const App: Component = () => {
     return (
